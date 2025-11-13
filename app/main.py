4729f366237fb053500aa903e7cc32b33045c40b
RPSIOErick <ecerickbastos@gmail.com>
2025-11-13 20:52:19 -0300

    Fix: Ajustando o readme

--- app/main.py
+++ app/main.py
@@ -6,6 +6,5 @@ app = FastAPI(title="ShadowCorp Anonymizer API")
 
 app.include_router(api_router, prefix="/api/v1")
 
-# Executa o servidor quando o arquivo for rodado diretamente
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
